d767d2b91dba7e71203fbf2466058bed1ecc42fd
Ayden Hafidi <aydenhafidi@gmail.com>
2025-05-09 01:34:27 -0400

    fixes again

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://aypx.github.io"
+        "https://aypx.github.io",
+        "https://aypx.github.io/Project10frontend"
     ]
 }));
 
